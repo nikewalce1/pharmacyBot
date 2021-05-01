c0003a023f3c0046533157fd4c523de043c210b8
nikewalce1 <danil123461@yandex.ua>
2021-05-01 22:40:23 +0300

    aded pars cards

--- Parser.py
+++ Parser.py
@@ -4,7 +4,7 @@ import json
 
 URL = 'https://366.ru/c/lekarstva/'
 HEADERS = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36'}
-#'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+HOST ='https://366.ru'
 
 def get_html(url, params=None):
     r = requests.get(url, headers=HEADERS,params=params)
@@ -12,25 +12,79 @@ def get_html(url, params=None):
 
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')  # формируются объекты
-    div = soup.find_all('a', class_='listing_product__title')
-    #td = soup.find_all('td')
+    card = soup.find_all('div', class_='listing_product')
 
-    percent = []
+    listOfCard = []
 
-    for i in range(len(div)):
-        a = ' '.join(div[i].get_text().split())
-        percent.append({
-            'name': a,
+    for item in card:
+        #a = ' '.join(div[i].get_text().split())
+        listOfCard.append({
+            'title': __Title(item.find('a', class_='listing_product__title')),
+            'manufacturer': __Manufacturer(item.find('div', class_='listing_product__manufacturer')),
+            'elipsis': __Elipsis(item.find('div', class_='i-text-ellipsis')),
+            'NumberOfPharmacies': __NumberOfPharmacies(item.find('div', class_='c-prod-item--product-text')),
+            'price': __Price(item.find('span', class_='price')),
+            'link': __Link(item.find('a', class_='listing_product__title')),
+            'image': __Image(item.find('img', class_='lazyload'))
         })
 
 
-    #print(percent)
     #data = json.dumps(percent, ensure_ascii=False)
     #load = json.loads(data)
     #encoding='utf-8'
     # with open("geo-wb-sklad.json", "w", encoding='utf-8') as file:
     #     file.write(data)
-    return percent
+    return listOfCard
+
+def __Image(item):
+    if item:
+        item = item.get('data-src')
+    else:
+        item = None
+    return item
+
+def __Link(item):
+    if item:
+        item = HOST + item.get('href')
+    else:
+        item = None
+    return item
+
+
+def __Price(item):
+    if item:
+        item = ' '.join(item.get_text().split())
+    else:
+        item = 'None'
+    return item
+
+def __Title(item):
+    if item:
+        item = ' '.join(item.get_text().split())
+    else:
+        item = 'None'
+    return item
+
+def __Manufacturer(item):
+    if item:
+        item = ' '.join(item.get_text().split())
+    else:
+        item = 'None'
+    return item
+
+def __Elipsis(item):
+    if item:
+        item = ' '.join(item.get_text().split())
+    else:
+        item = 'None'
+    return item
+
+def __NumberOfPharmacies(item):
+    if item:
+        item = ' '.join(item.get_text().split())
+    else:
+        item = 'None'
+    return item
 
 def parse():
     html = get_html(URL)
