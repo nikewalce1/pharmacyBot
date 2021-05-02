a14fcb1c6b10fd17f4600ef667010bc7d8fd97aa
nikewalce1 <danil123461@yandex.ua>
2021-05-02 21:33:12 +0300

    pars other count pages

--- Parser.py
+++ Parser.py
@@ -10,6 +10,14 @@ def get_html(url, params=None):
     r = requests.get(url, headers=HEADERS,params=params)
     return  r
 
+def get_pages_count(html):
+    soup = BeautifulSoup(html, 'html.parser')  # формируются объекты
+    pagination = soup.find_all('a',class_='b-pagination__item')
+    if pagination:
+        return int(pagination[-2].get_text())#срез(предпоследний элемент)
+    else:
+        return 1
+
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')  # формируются объекты
     card = soup.find_all('div', class_='listing_product')
@@ -27,7 +35,6 @@ def get_content(html):
             'link': __Link(item.find('a', class_='listing_product__title')),
             'image': __Image(item.find('img', class_='lazyload'))
         })
-
 
     #data = json.dumps(percent, ensure_ascii=False)
     #load = json.loads(data)
@@ -89,7 +96,12 @@ def __NumberOfPharmacies(item):
 def parse():
     html = get_html(URL)
     if html.status_code == 200:
-        print(get_content(html.text))
+        medications = []
+        pages_count = get_pages_count(html.text)
+        for page in range(1,pages_count + 1):
+            print(f'Парсинг страницы {page} из {pages_count}')
+            html = get_html(URL,params={'page':page})
+            medications.extend(get_content(html.text))#extend - расширяет список
     else:
         print('Error')
 parse()
